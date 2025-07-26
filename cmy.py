20f2b028287815f29ca2d180b5ae5dba6c47fac8
Ivandegoat <vania.miloserdov@gmail.com>
2025-07-26 15:35:14 -0400

    added movement
    also added bunch of app variables to change later. once u get to the end maze resets(need to write that later).

--- cmy.py
+++ cmy.py
@@ -2,28 +2,68 @@ from cmu_graphics import *
 import random
 
 def onAppStart(app):
-    app.rows = 21
-    app.cols = 31
+    app.startScreen = True
+    app.count = 0
+    app.rows = 17
+    app.cols = 17
     app.cellSize = 20
-    app.width = app.cols * app.cellSize
-    app.height = app.rows * app.cellSize
+    app.width = app.cols*app.cellSize
+    app.height = app.cellSize*app.rows
+    app.setMaxShapeCount(5000)
+    app.level = 1
+    app.playerRadius = app.cellSize*0.4
+    app.gameOver = False
     app.maze = [[1 for _ in range(app.cols)] for _ in range(app.rows)]
-
+    app.angle = 270
+    app.game3D = False
     startRow, startCol = 1, 1
     app.maze[startRow][startCol] = 0
     carveMaze(app, startRow, startCol)
 
-    app.maze[0][1] = 0  # entrance
+    
+
+
+    app.maze[0][1] = 0
     for col in range(app.cols - 2, 0, -1):
         if app.maze[app.rows - 2][col] == 0:
-            app.maze[app.rows - 1][col] = 0  # exit
+            app.maze[app.rows - 1][col] = 0
             app.exitCol = col
             break
-
-    # Player position in pixels (center of start cell)
+    app.start = (0, 1)
+    app.end = (app.rows - 1, app.exitCol)
     app.playerX = startCol * app.cellSize + app.cellSize / 2
     app.playerY = startRow * app.cellSize + app.cellSize / 2
-    app.playerRadius = app.cellSize * 0.4
+    app.speed = 3
+    app.cubes = []
+
+def onKeyHold(app, keys):
+    dx, dy = 0, 0
+    if 'a' in keys:
+        dx -= 5
+    if 'd' in keys:
+        dx += 5
+    if 'w' in keys:
+        dy -= 5
+    if 's' in keys:
+        dy += 5
+
+    newX = app.playerX + dx
+    newY = app.playerY + dy
+
+    
+    if isLegal(app, newX, newY):
+        app.playerX = newX
+        app.playerY = newY
+
+    col = int(app.playerX // app.cellSize)
+    row = int(app.playerY // app.cellSize)
+    app.cubes = [(r, c) for (r, c) in app.cubes
+                 if not (r == row and c == col)]
+    if row == app.rows - 1 and col == app.exitCol and len(app.cubes) == 0:
+        print("Maze completed! Generating a new one...")
+        app.level += 1
+        resetMaze(app)
+
 
 def carveMaze(app, startRow, startCol):
     stack = [(startRow, startCol)]  # fixed variable name
